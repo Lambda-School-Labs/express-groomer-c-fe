7047a4ba3d60604a2b6c6eb1ce9b034a0ba0f052
vishalicious213 <vishalicious213@gmail.com>
2021-03-18 15:46:17 -0700

    Vaccination image uploads to Cloudinary and saves to DB!

--- src/components/common/FileUpload.js
+++ src/components/common/FileUpload.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { Button } from 'antd';
 
 import './FileUpload.css';
 import { useOktaAuth } from '@okta/okta-react';
 
+// uploadUrl is the path to the DB endpoint in our API - it varies with what we're saving
+// fileUrl is the Cloudinary path where the file was saved
+
 const FileUpload = ({ uploadUrl }) => {
   const { authState } = useOktaAuth();
-  const [selectedFile, setSelectedFile] = useState();
-  const [fileUrl, setFileurl] = useState('');
+  // const [selectedFile, setSelectedFile] = useState();
+  // const [fileUrl, setFileurl] = useState('');
+  let selectedFile = '';
+  let fileUrl = '';
 
   const getAuthHeader = authState => {
     if (!authState.isAuthenticated) {
@@ -18,7 +24,9 @@ const FileUpload = ({ uploadUrl }) => {
   };
 
   const changeHandler = event => {
-    setSelectedFile(event.target.files[0]);
+    // setSelectedFile(event.target.files[0]);
+    selectedFile = event.target.files[0];
+    console.log('selectedFile', selectedFile);
   };
 
   const handleSubmission = async () => {
@@ -33,22 +41,26 @@ const FileUpload = ({ uploadUrl }) => {
         'https://api.cloudinary.com/v1_1/expressgroomer/image/upload',
         formData
       )
-      .then(res => setFileurl(res.data.secure_url))
+      // .then(res => setFileurl(res.data.secure_url))
+      .then(res => (fileUrl = res.data.secure_url))
       .then(res => console.log('Cloudinary response', res))
       .catch(error => console.log('Cloudinary error: ', error));
 
     console.log('file URL', fileUrl);
     console.log('upload URL', uploadUrl);
 
-    const rest = await axios.post(
-      `${process.env.REACT_APP_API_URI}/${uploadUrl}`,
-      { location: fileUrl },
-      {
-        headers,
-      }
-    );
+    axios
+      .post(
+        `${process.env.REACT_APP_API_URI}/${uploadUrl}`,
+        { location: fileUrl },
+        {
+          headers,
+        }
+      )
+      .then(res => console.log('DB response', res))
+      .catch(error => console.log('DB error', error));
 
-    rest();
+    // rest();
 
     console.log('file URL-2', fileUrl);
     // console.log({ rest }, 'Test Res');
