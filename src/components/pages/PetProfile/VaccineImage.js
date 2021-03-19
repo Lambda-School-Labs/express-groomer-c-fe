8109a696e040da333decb1bbca3c2b7c4ba56001
vishalicious213 <vishalicious213@gmail.com>
2021-03-18 20:07:35 -0700

    cleaned up code. left console.logs in FileUpload to test next upload components

--- src/components/pages/PetProfile/VaccineImage.js
+++ src/components/pages/PetProfile/VaccineImage.js
@@ -1,22 +1,19 @@
 import React, { useContext } from 'react';
 import { CustomersContext } from '../../../state/contexts/CustomersContext';
-import { PetsContext } from '../../../state/contexts/PetsContext';
 import FileUpload from '../../common/FileUpload';
 import { Divider, Row } from 'antd';
 import './pet.scss';
 
 const VaccineImage = props => {
   const { custInfo } = useContext(CustomersContext);
-  const { pets } = useContext(PetsContext);
   const thisPet = props.pet;
-
-  console.log('pets', pets);
 
   return (
     <div>
       <Divider style={{ borderColor: 'lightblue' }}>
         Upload Pet Vaccination Image
       </Divider>
+
       <Row justify={'center'}>
         <FileUpload
           uploadUrl={`pets/vaccination-upload/${thisPet}?customer_id=${custInfo.user_id}`}
