5a10f27f97423aa4e06fc6f29091c7bb6d1723ae
Henry Brophy <henrybrophy@henrys-mbp.lan>
2021-03-31 20:10:43 -0400

    Post Mortem.

--- src/components/pages/CustomerDashboard/FavoriteGroomers.js
+++ src/components/pages/CustomerDashboard/FavoriteGroomers.js
@@ -1,28 +1,16 @@
-import React, { useContext } from 'react';
-import { APIContext } from '../../../state/contexts/APIContext';
-import { GroomersContext } from '../../../state/contexts/GroomersContext';
+import React from 'react';
 import { Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
+
 import './overview.scss';
 
 // THIS FEATURE IS HARDCODED AND NEEDS TO BE CONNECTED TO THE FAV GROOMERS BACKEND.
 
 function FavoriteGroomers() {
-  const { getGroomerByID } = useContext(APIContext);
-  const { groomer } = useContext(GroomersContext);
-
-  const groomerTest = () => {
-    console.log('Running Groomer Test');
-    getGroomerByID('00ulthapbErVUwVJy4x6');
-    console.log(groomer, 'Groomer');
-    // console.log(groomerTest);
-  };
-  groomerTest();
-
   return (
     <div id="Groomers">
       <div className="fav-Groomers-container">
-        <p className="fav-Groomers-name">HC Groomer</p>
+        <p className="fav-Groomers-name">HC Groomers</p>
         <div className="pet-avatar">
           <Avatar size={100} icon={<UserOutlined />} />
         </div>
